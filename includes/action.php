388ef329b7618e214e20da79608f8856191d8ffa
TomGoldberg <tomg3210@gmail.com>
2016-02-01 23:06:15 +0200

    second commit

--- includes/action.php
+++ includes/action.php
@@ -13,14 +13,27 @@
 
         //use return daya
         while( $row = mysqli_fetch_assoc($result) ){
-            echo "<h2>".$row["r.name"]."</h2>";
-//            echo ("<section id="secwrap"><div id="d1"><h2>".$row["r.name"]."</h2></div></section>");
+//          echo "<h2>".$row["r.price"]."</h2>";
+//          echo ("<section id="secwrap"><div id="d1"><h2>".$row["r.name"]."</h2></div></section>");
+            echo "<section id='secwrap' style='background:url(images/".$row["r.pic"].") no-repeat;'>";
+            echo "<div id='d1'><h2>".$row["r.name"]."</h2></div>";
+            //echo "<div id='img1' style='background:url(images/".$row["r.pic"].") no-repeat;'></div>";
+            //echo "<div id='img1' style='background:url(images/robot2.png) no-repeat'></div>";
+            //echo "<div id='d2'><h4>".$row["r.price"]."$</h4></div>";
+            echo "</section>";
         }
 //        while($row = mysqli_fetch_row($result)){
 //            echo "<section>";
 //            var_dump($row);
 //            echo "</section>";
 //}
+
+
+//                <section id="secwrap">
+//                	<div id="d1"><h2>Chef Robot</h2></div>
+//                	<div id="img1"></div>
+//                	<div id="d2"><h4>230$</h4></div>
+//                </section>
 
         mysqli_free_result($result);
 		mysqli_close($connection);
@@ -35,4 +48,9 @@
                 <div id="d2"><h4>230$</h4></div>
             </section>
 
+-->
+<!--
+                <section id="secwrap" style='background:url(images/washer.png) no-repeat'>
+                    <div id="d1"><h2>Chef Robot</h2></div>
+                </section>
 -->
